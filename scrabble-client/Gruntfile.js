d2608a62a8d75d336f0b3ff2791b9bf176f8afd0
Laurie Boyes <laurie.boyes@informa.com>
2014-06-01 22:27:03 +0100

    Grunt brings in libraries and uglifies

--- scrabble-client/Gruntfile.js
+++ scrabble-client/Gruntfile.js
@@ -3,21 +3,45 @@ module.exports = function (grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            dist: {
+                src: ['lib/**/*.js', 'src/**/*.js'],
+                dest: 'build/js/scrabble-client.js'
+            }
+        },
         uglify: {
             options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                report: 'min'
             },
-            build: {
-                src: 'src/<%= pkg.name %>.js',
-                dest: 'build/<%= pkg.name %>.min.js'
+            dist: {
+                files: {
+                    'build/js/scrabble-client.min.js': [
+                        'build/js/scrabble-client.js'
+                    ]
+                }
+            }
+        },
+        processhtml: {
+            dist: {
+                options: {
+                    process: true,
+                    data: {
+                        title: 'Lrnk Scrabble',
+                        message: 'I wonder where this message shows up'
+                    }
+                },
+                files: {
+                    'build/index.html': ['src/index.html']
+                }
             }
         }
     });
 
-    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-processhtml');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['concat', 'uglify', 'processhtml']);
 
 };
