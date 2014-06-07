d9e85e60586669122f49d586926200f86c442ffa
Laurie Boyes <laurie.boyes@informa.com>
2014-06-07 13:34:46 +0100

    Update model from text box

--- scrabble-client/src/js/AppView.js
+++ scrabble-client/src/js/AppView.js
@@ -1,10 +1,22 @@
 App.AppView = Backbone.View.extend({
     el: '#main',
 
-    template: _.template("<h3>Hello. Apparently, <%= value %><h3>"),
+    template: _.template("<label for=\"testText\">Test me: </label><input type=\"text\" name=\"testText\" id=\"testText\"/>" +
+        "<h3>Hello <%= value %><h3>"),
 
     initialize: function(){
+        _.bindAll(this, "render");
+        this.model.bind('change', this.render);
         this.render();
+    },
+
+//    Elements need to be within el
+    events: {
+        "change input#testText": "updateThing"
+    },
+
+    updateThing: function(e) {
+        this.model.set("value", $(e.currentTarget).val());
     },
 
     render: function(){
