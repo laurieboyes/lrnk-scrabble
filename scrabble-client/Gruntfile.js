727c184665d37fc24d3447de6419be86fca97a02
Laurie Boyes <laurie.boyes@informa.com>
2014-06-15 13:17:29 +0100

    Added grunt task for moving built JS to webapp src

--- scrabble-client/Gruntfile.js
+++ scrabble-client/Gruntfile.js
@@ -2,52 +2,73 @@ module.exports = function (grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        concat: {
-            dist: {
-                src: [
-                    'lib/jquery.js',
-                    'lib/underscore.js',
-                    'lib/backbone.js',
-                    'lib/backbone-localstorage.js',
-                    'src/**/*.js'
-                ],
-                dest: 'build/js/scrabble-client.js'
-            }
-        },
-        uglify: {
-            options: {
-                report: 'min'
-            },
-            dist: {
-                files: {
-                    'build/js/scrabble-client.min.js': [
-                        'build/js/scrabble-client.js'
-                    ]
+            pkg: grunt.file.readJSON('package.json'),
+            concat: {
+                dist: {
+                    src: [
+                        'lib/jquery.js',
+                        'lib/underscore.js',
+                        'lib/backbone.js',
+                        'lib/backbone-localstorage.js',
+                        'src/**/*.js'
+                    ],
+                    dest: 'build/js/scrabble-client.js'
                 }
-            }
-        },
-        processhtml: {
-            dist: {
+            },
+            uglify: {
                 options: {
-                    process: true,
-                    data: {
-                        title: 'Lrnk Scrabble',
-                        message: 'I wonder where this message shows up'
-                    }
+                    report: 'min'
                 },
+                dist: {
+                    files: {
+                        'build/js/scrabble-client.min.js': [
+                            'build/js/scrabble-client.js'
+                        ]
+                    }
+                }
+            },
+            processhtml: {
+                dist: {
+                    options: {
+                        process: true,
+                        data: {
+                            title: 'Lrnk Scrabble',
+                            message: 'I wonder where this message shows up'
+                        }
+                    },
+                    files: {
+                        'build/index.html': ['src/index.html']
+                    }
+                }
+            },
+            clean: {
+                default: {
+                    src: ['../scrabble-server/scrabble/src/main/webapp/WEB-INF/scrabble'],
+                    options: {
+                        force: true
+                    }}
+            },
+            copy: {
                 files: {
-                    'build/index.html': ['src/index.html']
+                    cwd: 'build',  // set working folder / root to copy
+                    src: '**/*',           // copy all files and subfolders
+                    dest: '../scrabble-server/scrabble/src/main/webapp/WEB-INF/scrabble',    // destination folder
+                    expand: true           // required when using cwd
                 }
             }
         }
-    });
+    )
+    ;
 
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-processhtml');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-    // Default task(s).
+// Default task(s).
     grunt.registerTask('default', ['concat', 'uglify', 'processhtml']);
+    grunt.registerTask('deploy', ['concat', 'uglify', 'processhtml', 'clean', 'copy']);
 
-};
+}
+;
